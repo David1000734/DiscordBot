7369e25e8526297b883c07424c48ee0ccdfcac5c
David <lam.david459@yahoo.com>
2024-05-15 17:02:10 -0700

    Added play and queue
    * ffmpeg is required for music playing and queue to work. *
    Added play function however, only works if local file is within the same directory

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import discord
 from discord.ext import commands
+from discord import FFmpegPCMAudio
 
 # Import neccesary tokens
 import apikey as key
 
 intents = discord.Intents.all()
 intents.members = True
+
+# Dictionary, store our queue of songs
+queues = {}
+
+def check_queue(ctx, id):
+    # if id is in our dictionary, and it is non-null
+    if queues[id] != []:
+        # Non-null, play the next song
+        voice = ctx.guild.voice_client
+        source = queues[id].pop(0)      # pop from top of stack
+        player = voice.play(source)     # play song
 
 # Commands will be predicated with a '!', 
 # Enables all intents from developer portal
@@ -41,7 +53,11 @@ async def hello(ctx):
 async def join(ctx):
     if (ctx.author.voice):
         channel = ctx.message.author.voice.channel
-        await channel.connect()
+        voice = await channel.connect()
+        source = FFmpegPCMAudio('yaosobi.mp4')
+
+        await ctx.send("Now playing.")
+        player = voice.play(source)
     else:
         await ctx.send(ctx.message.author.name + " is not currently in a channel.")
 
@@ -52,5 +68,72 @@ async def leave(ctx):
         await ctx.send("I left the voice chat.")
     else:
         await ctx.send("Currently not in a voice chat. Unable to leave.")
+
+@client.command(pass_content = True)
+async def pause(ctx):
+    voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
+
+    if (voice.is_playing()):
+        voice.pause()
+    else:
+        await ctx.send("No audio playing.")
+
+@client.command(pass_content = True)
+async def resume(ctx):
+    voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
+
+    if (voice.is_paused()):
+        voice.resume()
+    else:
+        await ctx.send("No audio is paused.")
+
+@client.command(pass_content = True)
+async def stop(ctx):
+    voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
+
+    voice.stop()
+    await ctx.send("Audio has been stopped.")           # DEBUG
+
+@client.command(pass_content = True)
+async def play(ctx, arg):
+    voice = ctx.guild.voice_client
+    source = FFmpegPCMAudio(arg)
+
+    # Check to see if there is anything currently playing
+    if (voice.is_playing()):
+        # Is currently playing, place in queue.
+
+        # Get the id of the discord server
+        guild_id = ctx.message.guild.id
+
+        if guild_id in queues:
+            queues[guild_id].append(source)
+        else:
+            queues[guild_id] = [source]
+        
+        await ctx.send("Added to queue")
+    else:
+        # Not currently playing. So play the song
+
+        # After the initial run, check the function (check_queue) to
+        # see if there is any queues songs. If there is, play it, else end.
+        player = voice.play(source, \
+                            after = lambda x = None: check_queue(ctx, ctx.message.guild.id))
+
+# Remove this queue. One is present inside play...
+@client.command(pass_content = True)
+async def queue(ctx, arg):
+    voice = ctx.guild.voice_client
+    source = FFmpegPCMAudio(arg)
+
+    guild_id = ctx.message.guild.id
+
+    if guild_id in queues:
+        queues[guild_id].append(source)
+    else:
+        queues[guild_id] = [source]
+    
+    await ctx.send("Added %d to queue" % guild_id)
+# Remove this queue. One is present inside play...
 
 client.run(key.disc_token)
