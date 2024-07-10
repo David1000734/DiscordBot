5b157acad18a84eebb14818ec8943855ff23a0da
David <lam.david459@yahoo.com>
2024-07-09 20:11:19 -0700

    Remove reddit task
    Added a way to remove reddit task
    
    Next push should create a menu for the reddit command

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -12,7 +12,8 @@ class Reddit(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.reddit_post = []        # Store current posts
-        self.reddit_instance = None
+        self.reddit_instance = None  # The reddit API
+        self.reddit_Task = []        # Running total of tasks created
 
     # *************** Non-command/event Functions ***************
     async def init_Reddit(self):
@@ -68,6 +69,9 @@ class Reddit(commands.Cog):
 
         # Validate subreddits and check conditions
         try:
+            # Used to find out if task has already been created.
+            found = False
+
             # Ensure that it has been instantiated.
             if (self.reddit_instance == None):
                 await self.init_Reddit()
@@ -82,22 +86,63 @@ class Reddit(commands.Cog):
 
             # Ensure passed subreddit does not already have a task for it.
 
-        except:
-            channel = self.client.get_channel(key.disc_botSpam)
-            await channel.send("Error: Unknown or invalid subreddit provided: %s" % arg)
+            # Iterate through the task list
+            for idx, currSub in enumerate(self.reddit_Task):
+                # Search for a task with the that subreddit name
+                if (currSub.get_name() == arg):
+                    found = True
+                    break               # Exit loop
+                # if task_name = name, END
+            # For task list, END
+
+            # If one is found, error
+            if (found):
+                raise ValueError("Duplicate subreddit tasks is not allowed.")
+
+        except Exception as error:
+            await ctx.send("Subreddit: \"%s\" Error: %s" % (arg, error))
             sub_valid = False
 
-        # If a valid subreddit was provied, create a task
         if (sub_valid):
             # Create time loop. Continuously run this function.
-            current_tasks = self.client.loop.create_task(self.reddit_background(arg, 3, 5))
+            current_tasks = self.client.loop.create_task(self.reddit_background(sub_Name = arg, get_limit = 3, sleep_time = 5))
 
-            current_tasks.set_name(arg)
-            print("My name is %s" % current_tasks.get_name())
-            await asyncio.sleep(5)
+            current_tasks.set_name(arg)     # Set the name to be the same as the subreddit
+            self.reddit_Task.append(current_tasks)      # store to array
+        # If valid sub, END
 
-            current_tasks.cancel()
-            print("Task %s has been canceled" % current_tasks.get_name())
+    @commands.command()
+    async def removeSub(self, ctx, arg):
+        found = False
+
+        # Iterate through the task list
+        for idx, currSub in enumerate(self.reddit_Task):
+            # Search for a task with the that subreddit name
+            if (currSub.get_name() == arg):
+                # If one is found, remove it
+                currSub.cancel()
+
+                # Remove from the list
+                self.reddit_Task.remove(currSub)
+
+                await ctx.send("Successfully removed subreddit: \"%s\", from tasks." % arg)
+
+                found = True        # Set flag
+                break               # Exit loop
+        
+        if (not found):
+            await ctx.send("Subreddit: \"%s\" not found. Unable to remove." % arg)
+
+    @commands.command()
+    async def clearAll(self, ctx):
+
+        for currSub in self.reddit_Task:
+
+            await ctx.send("Removed subreddit: \"%s\", from tasks." % currSub.get_name())
+            currSub.cancel()
+        
+        # Clear list
+        self.reddit_Task.clear()
 
 async def setup(client):
     await client.add_cog(Reddit(client))
